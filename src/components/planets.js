83ca33eccdd98cb9ea96fa48d7dc330ab1d3f814
RamonSchmitt <ramon.schmitt@yahoo.com>
2017-09-19 14:41:35 +0200

    clean up

--- src/components/planets.js
+++ src/components/planets.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ChevronLeft from 'material-ui/svg-icons/navigation/chevron-left';
+import ChevronRight from 'material-ui/svg-icons/navigation/chevron-right';
 
 class Planets extends Component {
   content() {
-    if (this.props.stellarData) {
+    if (this.props.stellarData.length > 0) {
       const planetSystem = this.props.stellarData;
 
       return planetSystem.map((planet) => {
@@ -10,22 +13,40 @@ class Planets extends Component {
           <li key={planet.pl_name}>
             <h1>{planet.pl_name}</h1>
             <p>year</p>
-            <h1>{planet.pl_orbper}</h1>
+            <h1>{planet.pl_orbper.toFixed(0)} earth days</h1>
           </li>
         );
       });
     }
-    return <h1>-</h1>;
+
+    return (
+      <li>
+        <h1>-</h1>
+      </li>
+    );
   }
 
   render() {
     return (
-      <div>
-        <p>known planets</p>
-        {}
-        <ul>
-          {this.content()}
-        </ul>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <div style={{ flexGrow: 1 }}>
+          <FloatingActionButton disabled={true}>
+            <ChevronLeft />
+          </FloatingActionButton>
+        </div>
+
+        <div style={{ flexGrow: 1 }}>
+          <p>known planets</p>
+          <ul>
+            {this.content()}
+          </ul>
+        </div>
+
+        <div style={{ flexGrow: 1 }}>
+          <FloatingActionButton disabled={true}>
+            <ChevronRight />
+          </FloatingActionButton>
+        </div>
       </div>
     );
   }
--- src/containers/star_system.js
+++ src/containers/star_system.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 
 import Star from '../components/star';
 import Planets from '../components/planets';
-
+import Divider from 'material-ui/Divider';
 
 class StarSystem extends Component {
   renderPlanetList(planets) {
@@ -20,6 +20,7 @@ class StarSystem extends Component {
     return (
       <div>
         <Star stellarData={stellarData} />
+        <Divider />
         <Planets stellarData={stellarData} />
       </div>
     );
